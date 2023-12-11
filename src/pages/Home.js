bd1175f30e29d2cf4ca014c7a5c4c2afe095997e
jorell raka <jorellraka@gmail.com>
2023-12-11 14:13:19 +1100

    This commit modifies the intro and add links to icon and resume

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,44 @@
 import React from "react";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import EmailIcon from "@material-ui/icons/Email";
+import InstagramIcon from "@material-ui/icons/Instagram"
 import GithubIcon from "@material-ui/icons/GitHub";
 import "../styles/Home.css";
 
 function Home() {
+  const linkedinProfileUrl = "https://www.linkedin.com/in/jorell-raka-b935a7199/";
+  const emailAddress = "mailto:jorellraka@gmail.com";
+  const instagramProfileUrl = "https://www.instagram.com/jorellraka/";
+  const resumePdfUrl = ".../Jorellresume.pdf"; 
+
   return (
     <div className="home">
       <div className="about">
-        <h2> Hi, My Name is Pedro</h2>
+        <h2> Hello! My Name is Jorell Tanusaputra</h2>
         <div className="prompt">
-          <p>A software developer with a passion for learning and creating.</p>
-          <LinkedInIcon />
-          <EmailIcon />
-          <GithubIcon />
+        <p>
+            I am an aspiring IT professional with a degree in Forensic Science (Digital Forensic), a Diploma in Innovation, and
+            certification in COMPTIA Security +. I have experience as a full stack developer intern at Infosys, which deepened my appreciation
+            for IT.
+          </p>
+          <div className="socialMedia">
+            <a href={linkedinProfileUrl} target="_blank" rel="noopener noreferrer">
+              <LinkedInIcon />
+            </a>
+            <a href={emailAddress} target="_blank" rel="noopener noreferrer">
+              <EmailIcon />
+            </a>
+            <a href={instagramProfileUrl} target="_blank" rel="noopener noreferrer">
+              <InstagramIcon />
+            </a>
+          </div>
+          <p>
+            Click{" "}
+            <a href={resumePdfUrl} download>
+              here
+            </a>{" "}
+            to download my resume
+          </p>
         </div>
       </div>
       <div className="skills">
